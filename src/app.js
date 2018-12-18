4dbab45754cafdbcb4540a2646fa97958b4cdafc
Jiwon Kang <jiwonk42@gmail.com>
2018-12-18 14:20:03 -0800

    add app and template objects app: properties include title, subtitle, and options array, template: renders properties of app object

--- src/app.js
+++ src/app.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const app = {
+    title: 'Indecision App',
+    subtitle: 'Put your life in the hands of a computer',
+    options: ['One', 'Two']
+};
+
 const template = (
     <div>
-    Hi
+        <h1>{app.title}</h1>
+        <p>{app.subtitle}</p>
+
+        <ol>
+            <li>Item one</li>
+            <li>Item two</li>
+        </ol>
     </div>
 );
 

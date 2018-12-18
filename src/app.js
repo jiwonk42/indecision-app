35c15cfb6c8b5db28927bc3b2086234fb249a6c5
Jiwon Kang <jiwonk42@gmail.com>
2018-12-18 14:30:28 -0800

    apply conditional rendering techniques to object and component: if statements, ternary operators, logical && operator, conditionally rendering subtitle and option paragraph to the browser

--- src/app.js
+++ src/app.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// if statements
+// ternary operators
+// logical && operator
+
+// only render the subtitle (and p tag) if subtitle exist - logical && operator
+// render new p tag - if options.length > 0 "Here are your options" "No options"
+
 const app = {
     title: 'Indecision App',
     subtitle: 'Put your life in the hands of a computer',
@@ -10,8 +17,9 @@ const app = {
 const template = (
     <div>
         <h1>{app.title}</h1>
-        <p>{app.subtitle}</p>
+        {app.subtitle && <p>{app.subtitle}</p>}
 
+        <p>{app.options.length > 0 ? 'Here are options' : 'No options'}</p>
         <ol>
             <li>Item one</li>
             <li>Item two</li>

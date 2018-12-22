0cd0649fc2c3bae1b11eea5c7460c550e4fa19bd
Jiwon Kang <jiwonk42@gmail.com>
2018-12-21 17:21:24 -0800

    disable What should I do button when there are no options listed

--- src/app.js
+++ src/app.js
@@ -40,7 +40,7 @@ const render = () => {
             <h1>{app.title}</h1>
             {app.subtitle && <p>{app.subtitle}</p>}
             <p>{app.options.length > 0 ? 'Here are options' : 'No options'}</p>
-            <button onClick={onMakeDecision}>What should I do?</button>
+            <button disabled={app.options.length === 0} onClick={onMakeDecision}>What should I do?</button>
             <button onClick={onRemoveAll}>Remove All</button>
             <ol>
                 {

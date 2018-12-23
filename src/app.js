f0834d24b09e72e55489e8d7e8540ee7d765da70
Jiwon Kang <jiwonk42@gmail.com>
2018-12-22 20:46:00 -0800

    add handlePick function that alerts some message when what-should-I-do button is clicked

--- src/app.js
+++ src/app.js
@@ -30,10 +30,14 @@ class Header extends React.Component {
 }
 
 class Action extends React.Component {
+    handlePick() {
+        alert('handlePick');
+    }
+
     render() {
         return (
             <div>
-                <button>What should I do?</button>
+                <button onClick={this.handlePick}>What should I do?</button>
             </div>
         );
     }

7456353a0f7b5d5ec692c049d986b9644e026943
Jiwon Kang <jiwonk42@gmail.com>
2018-12-26 15:27:48 -0800

    add hasOptions attribute to Action component that disables button if there's no data in the options array

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ class IndecisionApp extends React.Component {
         return (
             <div>
                 <Header title={title} subtitle={subtitle} />
-                <Action />
+                <Action hasOptions={this.state.options.length > 0} />
                 <Options options={this.state.options} />
                 <AddOption />
             </div>
@@ -43,7 +43,12 @@ class Action extends React.Component {
     render() {
         return (
             <div>
-                <button onClick={this.handlePick}>What should I do?</button>
+                <button 
+                    onClick={this.handlePick}
+                    disabled={!this.props.hasOptions}
+                >
+                    What should I do?
+                </button>
             </div>
         );
     }

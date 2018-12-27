e3b81dcadfe3e9c4bb6e715c020db16bb8276722
Jiwon Kang <jiwonk42@gmail.com>
2018-12-26 16:13:21 -0800

    adds error handling to IndecisionApp component where it displays an error message in the AddOption component depending on the type of error

--- src/app.js
+++ src/app.js
@@ -27,6 +27,14 @@ class IndecisionApp extends React.Component {
     }
 
     handleAddOption(option) {
+        // error messages
+        if (!option) {
+            return 'Enter valid value to add item';
+        } else if (this.state.options.indexOf(option) > -1) {
+            return 'This option already exists';
+        }
+
+        // no errors
         this.setState((prevState) => {
             return {
                 options: prevState.options.concat([option])
@@ -110,24 +118,31 @@ class AddOption extends React.Component {
     constructor(props) {
         super(props);
         this.handleAddOption = this.handleAddOption.bind(this);
+        this.state = {
+            error: undefined
+        };
     }
 
     handleAddOption(e) {
         e.preventDefault();
 
         const option = e.target.elements.option.value.trim();
+        const error = this.props.handleAddOption(option);
 
-        if (option) {
-            this.props.handleAddOption(option);
-        }
+        this.setState(() => { // sets an error message by error type
+            return {
+                error
+            };
+        });
     }
 
     render() {
         return (
             <div>
+                {this.state.error && <p>{this.state.error}</p>}
                 <form onSubmit={this.handleAddOption}>
                     <input type="text" name="option" />
-                    <button>Add Option</button>
+                    <button>Add Option</button>    
                 </form>
             </div>
         );

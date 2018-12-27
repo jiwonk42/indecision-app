11bfb2c19d5cb7c7684acdd205aff61579d4e874
Jiwon Kang <jiwonk42@gmail.com>
2018-12-27 15:39:20 -0800

    set a condition where it produces a message when there's no option added

--- src/app.js
+++ src/app.js
@@ -119,6 +119,7 @@ const Options = (props) => {
     return (
         <div>
             <button onClick={props.handleDeleteOptions}>Remove All</button>
+            {props.options.length === 0 && <p>Please add an option</p>}
             {
                 // mapping through Option component; optionText is props that will be passed down to Option component
                 props.options.map((option) => 

415874e139c2e7bb84d361a8e03a656d0bd1a480
Jiwon Kang <jiwonk42@gmail.com>
2019-01-03 15:55:45 -0800

    move AddOption component to its own file, imported and exported

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import AddOption from './components/AddOption';
 
 class IndecisionApp extends React.Component {
     constructor(props) {
@@ -145,45 +146,6 @@ const Option = (props) => {
             >remove</button>
         </div>
     );
-};
-
-class AddOption extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handleAddOption = this.handleAddOption.bind(this);
-        this.state = {
-            error: undefined
-        };
-    }
-
-    handleAddOption(e) {
-        e.preventDefault();
-
-        const option = e.target.elements.option.value.trim();
-        const error = this.props.handleAddOption(option);
-
-        // sets an error message by error type
-        this.setState(() => ({
-            error
-        }));
-
-        // wipes input data if valid data is entered
-        if (!error) {
-            e.target.elements.option.value = '';
-        }
-    }
-
-    render() {
-        return (
-            <div>
-                {this.state.error && <p>{this.state.error}</p>}
-                <form onSubmit={this.handleAddOption}>
-                    <input type="text" name="option" />
-                    <button>Add Option</button>    
-                </form>
-            </div>
-        );
-    }
 };
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));

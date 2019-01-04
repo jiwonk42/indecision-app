4fdeff53d4016df02287fef3dfbaa467a664982e
Jiwon Kang <jiwonk42@gmail.com>
2019-01-04 08:40:43 -0800

    display a basic modal in the IndecisionApp component

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -2,9 +2,13 @@ import React from 'react';
 import Modal from 'react-modal';
 
 const OptionModal = () => (
-    <div>
-    some text
-    </div>
+    // Modal requires 2 props: isOpen, contentLabel
+    <Modal
+        isOpen={true}
+        contentLabel="Selected Option"
+    >
+        <h3>Selected Option</h3>
+    </Modal>
 );
 
 export default OptionModal;

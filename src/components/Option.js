6bdc6ec1239232ab35e613a4d3fa3feafe2307d9
Jiwon Kang <jiwonk42@gmail.com>
2019-01-04 09:06:42 -0800

    refactor other stateless functional components by removing return keywords and curly braces

--- src/components/Option.js
+++ src/components/Option.js
@@ -1,16 +1,14 @@
 import React from 'react';
 
-const Option = (props) => {
-    return (
-        <div>
-            Option: {props.optionText}
-            <button 
-                onClick={(e) => {
-                    props.handleDeleteOption(props.optionText); // passes an option text instead of option event object
-                }}
-            >remove</button>
-        </div>
-    );
-};
+const Option = (props) => (
+    <div>
+        Option: {props.optionText}
+        <button 
+            onClick={(e) => {
+                props.handleDeleteOption(props.optionText); // passes an option text instead of option event object
+            }}
+        >remove</button>
+    </div>
+);
 
 export default Option;
--- src/components/Options.js
+++ src/components/Options.js
@@ -1,23 +1,21 @@
 import React from 'react';
 import Option from './Option';
 
-const Options = (props) => {
-    return (
-        <div>
-            <button onClick={props.handleDeleteOptions}>Remove All</button>
-            {props.options.length === 0 && <p>Please add an option</p>}
-            {
-                // mapping through Option component; optionText is props that will be passed down to Option component
-                props.options.map((option) => 
-                    <Option 
-                    key={option} 
-                    optionText={option} 
-                    handleDeleteOption={props.handleDeleteOption}
-                    />
-                ) 
-            }
-        </div>
-    );
-};
+const Options = (props) => (
+    <div>
+        <button onClick={props.handleDeleteOptions}>Remove All</button>
+        {props.options.length === 0 && <p>Please add an option</p>}
+        {
+            // mapping through Option component; optionText is props that will be passed down to Option component
+            props.options.map((option) => 
+                <Option 
+                key={option} 
+                optionText={option} 
+                handleDeleteOption={props.handleDeleteOption}
+                />
+            ) 
+        }
+    </div>
+);
 
 export default Options;

c80d120e1fa2059ad4218e6241e094f4d747c4b3
Jiwon Kang <jiwonk42@gmail.com>
2019-01-06 16:54:18 -0800

    add styling to button and button--link

--- src/components/Option.js
+++ src/components/Option.js
@@ -4,6 +4,7 @@ const Option = (props) => (
     <div>
         Option: {props.optionText}
         <button 
+            className="button button--link"
             onClick={(e) => {
                 props.handleDeleteOption(props.optionText); // passes an option text instead of option event object
             }}
